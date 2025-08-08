562802df4fe86fae61ac887616f4bac2464bfa24
waleedalfar <waleedalfar1024@gmail.com>
2025-08-07 18:57:32 -0700

    Initialized webpage

---
+++ src/main/java/waleedalfar/store/demo1/HomeController.java
@@ -0,0 +1,12 @@
+package waleedalfar.store.demo1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index () {
+        return "index.html";
+    }
+}
---
+++ src/test/java/waleedalfar/store/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package waleedalfar.store.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
